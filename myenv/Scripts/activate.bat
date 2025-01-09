0572510747284801f1dfe618b1fd3cecb1219ee1
Klyne <superwow175@gmail.com>
2025-01-09 22:30:03 +0800

    Revamped Responsiveness (INCOMPLETE)

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Text-Summarizer-and-Grader-1\myenv
+set "VIRTUAL_ENV=E:\Text-Summarizer-and-Grader-1\myenv"
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -24,8 +24,8 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(myenv) 
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+set "VIRTUAL_ENV_PROMPT=(myenv) "
 
 :END
 if defined _OLD_CODEPAGE (
